ecebe5adbaaef26a5454e320e97dd38d8af53634
SrideviSupriya <supriyams1999@gmail.com>
2020-12-11 18:28:42 +0530

    First Commit

---
+++ Repository/IProceedToBuyRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ProceedToBuyService.Models;
+
+namespace ProceedToBuyService.Repository
+{
+    public interface IProceedToBuyRepository
+    {
+        public IEnumerable<WishlistDto> addToWishlist(WishlistDto entity);
+        public IEnumerable<CartDto> addToCart(CartDto entity);
+    }
+}
